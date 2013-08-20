fb057a84be4ac6358ce3328cb6343126f0d07482
Mohamed Omar <mohamed12@live.co.uk>
2013-08-20 18:10:12 +0100

    Added Time to the HUD

---
+++ src/com/redomar/game/lib/Time.java
@@ -0,0 +1,18 @@
+package com.redomar.game.lib;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Time {
+
+	public Time(){
+		
+	}
+	
+	public String getTime(){
+		Calendar cal = Calendar.getInstance();
+    	cal.getTime();
+    	SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+    	return( sdf.format(cal.getTime()) );
+	}
+}
