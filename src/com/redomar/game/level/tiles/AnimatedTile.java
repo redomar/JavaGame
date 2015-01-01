70b6d2bbb1c23b2427a3893c08fefd7299cc4355
Mohamed Omar <mohamed12@live.co.uk>
2015-01-01 01:54:48 +0000

    Added some Javadoc comments for types of tiles

--- src/com/redomar/game/level/tiles/AnimatedTile.java
+++ src/com/redomar/game/level/tiles/AnimatedTile.java
@@ -7,6 +7,14 @@ public class AnimatedTile extends BasicTile {
 	private long lastIterationTime;
 	private int animationSwitchDelay;
 
+	/**
+	 * 
+	 * @param id Unique ID for the Tile
+	 * @param animationCoords A 2D array of all x,y-coordinates in Integers.
+	 * @param tileColour Colours from the SpriteSheet.
+	 * @param levelColour Colours to be displayed in the Game World.
+	 * @param animationSwitchDelay Length of time to be delayed to the next frame from the 2D array.
+	 */
 	public AnimatedTile(int id, int[][] animationCoords, int tileColour,
 			int levelColour, int animationSwitchDelay) {
 		super(id, animationCoords[0][0], animationCoords[0][1], tileColour,
--- src/com/redomar/game/level/tiles/BasicSolidTile.java
+++ src/com/redomar/game/level/tiles/BasicSolidTile.java
@@ -2,6 +2,15 @@ package com.redomar.game.level.tiles;
 
 public class BasicSolidTile extends BasicTile {
 
+	/**
+	 * This is a solid tile, but does not emit any light. This tile does not have any animations.
+	 * 
+	 * @param id Unique ID for the Tile
+	 * @param x Specifies the x-coordinate from the SpriteSheet, measured in 8 pixels. Must be 0 - 31
+	 * @param y Specifies the y-coordinate from the SpriteSheet, measured in 8 pixels. Must be 0 - 31
+	 * @param tileColour Colours from the SpriteSheet.
+	 * @param levelColour Colours to be displayed in the Game World.
+	 */
 	public BasicSolidTile(int id, int x, int y, int tileColour, int levelColour) {
 		super(id, x, y, tileColour, levelColour);
 		this.solid = true;
